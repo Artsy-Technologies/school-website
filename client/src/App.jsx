5ab0d5946a38bf4a43b83e5c8ac214dc7893cebf
RaiDevX8 <charanrai193@gmail.com>
2024-07-15 22:51:23 +0530

    Add : FAQ and Carousel to Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Home/>
+    </div>
   )
 }
 
---
+++ client/src/components/Carousel/Data.js
@@ -0,0 +1,33 @@
+ const data = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1591123120675-6f7f1aae0e5b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quas harum atque amet',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1577896851231-70ef18881754?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quas harum atque amet',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1571260899304-425eee4c7efc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title:
+      'consectetur adipisicing elit. Excepturi quas harum atque amet',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1591123120675-6f7f1aae0e5b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title:
+      'sit amet consectetur adipisicing elit. Excepturi quas harum atque amet',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1591123120675-6f7f1aae0e5b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title:
+      ' ipsum dolor sit amet consectetur adipisicing elit. Excepturi quas harum atque amet',
+  },
+]
+export default data;
