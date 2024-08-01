69b8cecc3726d7be677f3ad5a33a806b5d59a9b6
Sheethal KV <sheethalkv24@gmail.com>
2024-08-01 10:59:18 +0530

    [Feat]: ResPponsive Footer

--- client/src/components/header/Navbar.jsx
+++ client/src/components/header/Navbar.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <header className="bg-purple-500 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold">
-          <h1>School Name</h1>
-        </div>
-        <nav>
-          <ul className="flex space-x-4">
-            <li><Link to="/" className="hover:underline">Home</Link></li>
-            <li><Link to="/about" className="hover:underline">About</Link></li>
-            <li><Link to="/contact" className="hover:underline">Contact</Link></li>
-            <li><Link to="/other" className="hover:underline">Other</Link></li>
+    <header className="flex justify-between items-center bg-purple-500 text-white p-4 drop-shadow-md">
+      
+          <h1 className="px-30">SRI MURUGHA  RAJENDRASWAMY CBSE SCHOOL</h1>
+    
+    
+          <ul className=" flex justify-between items-end flex space-x-10">
+            <li className="p-3 hover:bg-purple-400 rounded-md transition-all cursor-pointer"><Link to="/HomePage" className="hover:underline">Home</Link></li>
+            <li className="p-3 hover:bg-purple-400 rounded-md transition-all cursor-pointer"><Link to="/AboutPage" className="hover:underline">About Us</Link></li>
+            <li className="p-3 hover:bg-purple-400 rounded-md transition-all cursor-pointer"><Link to="/AcademicsPage" className="hover:underline">Academics</Link></li>
+            <li className="p-3 hover:bg-purple-400 rounded-md transition-all cursor-pointer"><Link to="/Admissionpage" className="hover:underline">Admission</Link></li>
+            <li className="p-3 hover:bg-purple-400 rounded-md transition-all cursor-pointer"><Link to="/StudentPage" className="hover:underline">Student Life</Link></li>
+            <li className="p-3 hover:bg-purple-400 rounded-md transition-all cursor-pointer"><Link to="/contact" className="hover:underline">Contact</Link></li>
           </ul>
-        </nav>
-      </div>
+        
+          <i className="bx bx-menu x1:hidden block text-5xl cursor-pointer"></i>
+      
     </header>
   )
 }
