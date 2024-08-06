c735e62c0b3200ae4fcb4c8af3515c711ad12674
RaiDevX8 <charanrai193@gmail.com>
2024-08-06 17:06:26 +0530

    fix: banner component

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -6,7 +6,7 @@ import image from './image.svg'
 function Banner({ main, content, buttonText }) {
   return (
     <section className="relative  m-5 bg-white rounded-xl border-2 border-solid shadow-sm border-neutral-800 ">
-      <div className="absolute top-0 left-0 p-5 w-1/3 md:w-1/5 md:w-[200px] lg:w-[150px] ">
+      <div className="absolute top-0 left-0 p-5 w-md:w-1/3 lg:w-[150px] ">
         <img
           loading="lazy"
           src={image}
