68571f40e91fd68ae49dc3c6e14c7c90ea6db853
930-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2024-08-18 16:08:06 +0530

    update

--- client/src/pages/dashboard/AdminPage.jsx
+++ client/src/pages/dashboard/AdminPage.jsx
@@ -1,12 +1,46 @@
 /* eslint-disable no-unused-vars */
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAdmin } from '../../hooks/AdminContext';
 import Sidebar from '../../components/admin/Sidebar';
+import ContactCard from '../../components/admin/ContactCard';
+import axios from 'axios';
+
 function AdminPage() {
+    const [contactData, setContactData] = useState([]);
+
+    const getContactData = async () => {
+        try {
+            let response = await axios.get('/api/students/getContactStudentsData');
+            setContactData(response?.data?.allContactData);
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+
+
+    useEffect(() => {
+        getContactData();
+    }, [])
 
     return (
-        <section className="h-screen  " >
-            <Sidebar />
+        <section className="h-screen overflow-y-hidden flex " >
+            <div className='w-[20%] h-full ' >
+                <Sidebar />
+            </div>
+
+            <div className='w-[80%] h-full border-2  ' >
+                <div className="">
+                    {
+                        contactData.map((contactData,i) => (
+                            <ContactCard key={i} contactData={contactData}  />
+                        ))
+                    }
+                </div>
+            </div>
+
         </section>
     )
 }
