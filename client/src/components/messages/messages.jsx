d655d35b0c98c23e5a835170855d4cc89021eeaa
srashtibhaoriya <srashtibhadoriya4@gmail.com>
2024-09-09 22:59:06 +0530

    messages updated

--- client/src/components/messages/messages.jsx
+++ client/src/components/messages/messages.jsx
@@ -32,15 +32,9 @@ export const Messages = () => {
     </p>
   </div>
   <p className="text-black dark:text-white text-md leading-relaxed pt-10">
-    Our focus at school shall be; “Emphasis on building strong foundational
-    Basics” Establishing strong foundational basics is the key when it comes
-    to early childhood development. Promote liberal thinking; Critical
-    thinking is inculcated at the primary level that broadens the knowledge
-    horizons of the students. Students are encouraged to be more
-    individualistic with a global perspective of education and community.
-    Additionally, students are expected to take an active role in designing
-    their own learning activities. We believe in developing cooperative
-    skills over competitive.
+  “J.K. Memorial (CBSE) Central School is here to create Ideals for the society” I, Dr. Mallikarjun as the President of J.K. Memorial (CBSE) Central School, Kamenahalli, Koratagere, Tumkur, whole-heartedly welcome you all.
+Our mission is to Provide English education with affordable fee structure in the rural parts of Koratagere region, Tumkur. So, we had CBSE affiliation during the academic year-2011-12, we laid the foundation of J.K. Memorial (CBSE) Central School, to recognize the uniqueness of each child and to nurture the treasured skills & talent in them. Education is a complete process that leads to the attainment of the full potential of the child. Our focus is to develop our students as global citizen with tolerance respect and appreciation of diverse cultures and religions for a learning experience. They should be self-motivated, independent, confident, decision makers to take a leadership role in future at JKMCS, we arm our students with technological supremacy and help them integrated it with values, morals and our cultural legacy. JKMCS CBSE School is a community that puts its core values into practice. We respect student’s capacity to think independently, produce quality work and maintain their part in the learning process and expect them to do their best. I believe great things are store in the JKMCS School as we live out our mission with uttermost dedication. Thank you for our parents, well-wishers and all stakeholders for supporting us for accompanying in our dream.
+
   </p>
 </div>
 
@@ -60,15 +54,7 @@ export const Messages = () => {
     </p>
   </div>
   <p className="text-black dark:text-white text-md leading-relaxed pt-10">
-    Our focus at school shall be; “Emphasis on building strong foundational
-    Basics” Establishing strong foundational basics is the key when it comes
-    to early childhood development. Promote liberal thinking; Critical
-    thinking is inculcated at the primary level that broadens the knowledge
-    horizons of the students. Students are encouraged to be more
-    individualistic with a global perspective of education and community.
-    Additionally, students are expected to take an active role in designing
-    their own learning activities. We believe in developing cooperative
-    skills over competitive.
+   I’m very much eager to welcome to J.K. Memorial (CBSE) Central School, Koratagere, Tumkur. “Our school endeavors to inspire and empower our students to be lifelong learners, critical thinkers and productive members of an ever-changing global society. In JKMCS, students are encouraged to channelize their potential in the pursuit of excellence in a holistic, student-centered environment. Now in its fourth year, JKMCS continues to move forward in the true spirit of ability driven education while sensitizing its students to the needs of the community besides inculcating values like integrity, fortitude and acceptance of individual differences. The likelihood of achieving this is strengthened by the fact that we offer an academic program that facilitates an overall development of the human personality in all its dimensions-intellectual, physical, social, moral and spiritual. JKMCS CBSE SCHOOL’s vision is to give the students the al-round capability including creativity, critical thinking, analyzing skills, observation and knowledge empowerment leading to the generation of excellent, performing citizens with sterling character. We, at JKMCS CBSE School, is used to remind students & parents “Responsibility, Integrity, Caring, Respectful, Academic and Cooperative”. These values, we believe, will enable our students to take a leadership role in learning and in life. There is a commitment to an integrated curriculum approach through inquiry learning. The development of higher order thinking skills, risk taking, adaptability, cooperation, and communication techniques are fostered. JKMCS CBSE School provides a platform to its students to showcase their inborn talents. We also take utmost care in mold our children as responsible global citizen. We are committed to the improvement and growth of student community. We would like to see them soar to new heights and taste success in all their endeavors. We are convinced that the education’s purpose is to replace an empty mind with an open one. I expect sincere cooperation from parents and guardians for the attainment of school’s objectives.
   </p>
 </div>
 
